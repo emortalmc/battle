f9ff48ea3b7207b382410463a5f076d158efb07c
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2024-01-05 01:14:10 +0000

    (deps): bump java-game-sdk + minestom-core

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.emortal.minestom:game-sdk:52c9b1d")
+    implementation("dev.emortal.minestom:game-sdk:8359368")
 
     implementation("dev.hollowcube:polar:1.3.1")
     implementation("com.github.EmortalMC:MinestomPvP:6aefcba403")
